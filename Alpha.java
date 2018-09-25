19a7c54360272334c00f4b6780a256e239543e1c
Irfana14 <irfusadhik@gmail.com>
2018-09-25 09:26:05 +0530

    Create Alpha.java

---
+++ Alpha.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+import java.util.Scanner;
+class Alpha
+{
+	public static void main(String[ ] arg)
+	{
+
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a character : ");
+	char ch=sc.next( ).charAt(0);	 
+	//char ch=sc.nextChar();
+	
+	if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+	System.out.println("Alphabet                ");
+
+		else
+		System.out.println("invalid");		
+	}
+
+}
