a84d44b064388ea48d0cb792426d7468f1549030
Irfana14 <irfusadhik@gmail.com>
2018-12-18 09:59:18 +0530

    Create CND.java

---
+++ CND.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CND { 
+
+	static int countDigit(int n) 
+	{ 
+		int count = 0; 
+		while (n != 0) { 
+			n = n / 10; 
+			++count; 
+		} 
+		return count; 
+	} 
+
+	/* Driver program to test above function */
+	public static void main(String[] args) 
+	{ 
+		Scanner n = new Scanner(System.in);
+		int num = n.nextInt();
+	
+		System.out.print("Number of digits : " + countDigit(num)); 
+	} 
+} 
+// This code is contributed by Arnav Kr. Mandal. 
