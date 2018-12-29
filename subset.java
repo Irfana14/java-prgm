895a1251b949fa50ce17cd02c67337adedf1ad1b
Irfana14 <irfusadhik@gmail.com>
2018-12-29 18:32:21 +0530

    Update subset.java

--- subset.java
+++ subset.java
@@ -5,55 +5,33 @@ import java.lang.*;
 
  class subset
 {
-	static void tofind(int[] arr1, int[] arr2)
-	{
-		int a=arr1.length;
-		int b=arr2.length;
-		int i=0;	
-		int flag=0;
-		while(i<b)
-		{
-			flag=0;
-			for(int j=0;j<a;j++)
-			{
-				if(arr2[i]==arr1[j])
-				{
-					flag=1;
-					break;
-				}
-			}
-			if(flag==0)
-			{
-				System.out.println("NO");
-				break;
-			}
-			i++;
-		}
-		
-		if(flag==1)
-			System.out.println("YES");
-
-	}
-	public static void main(String[] args)
-	{
-			Scanner s = new Scanner(System.in);
-		int a =s.nextInt();
-		int b=s.nextInt();
-		int[] arr1=new int[a];
-		int[] arr2=new int[b];
-		int i=0;
-		
-		while(i<a)
-		{
-			arr1[i]=sn.nextInt();
-			i++;
-		}
-		i=0;
-			while(i<b)
-		{
-			arr2[i]=sn.nextInt();
-			i++;
-		}
-		tofind(arr1,arr2);		
-	}	
-	}
+	public static void main(String aa[]){
+     int n;
+     int l;
+     int c=0;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     l=s.nextInt();
+     int a[]=new int[n];
+     int b[]=new int[l];
+    for(int i=0;i<n;i++){
+        a[i]=s.nextInt();
+    }
+     for(int i=0;i<l;i++){
+        b[i]=s.nextInt();
+    }
+     for(int i=0;i<n;i++){
+         for(int j=0;j<l;j++){
+             if(a[i]==a[j]){
+                 c=1;
+                 break;
+             }
+         }
+     }
+     if(c==1){
+         System.out.println("Yes");
+     }
+ else{
+          System.out.println("No");
+ }
+    }}
